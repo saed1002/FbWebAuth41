e3be3888e3414bc7a5fc15a001a083e373ba6fd6
saed1002 <84580304+saed1002@users.noreply.github.com>
2021-05-25 21:08:08 -0500

    create conection to firebase

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyCXpYchPeOp673REcm6wqffF_ZoXv6mZSo",
+    authDomain: "fbwebauth41-1.firebaseapp.com",
+    projectId: "fbwebauth41-1",
+    storageBucket: "fbwebauth41-1.appspot.com",
+    messagingSenderId: "1086080137003",
+    appId: "1:1086080137003:web:f41d48a8129ac21bf0b76e",
+    measurementId: "G-JGQKRPYVDM"
+  });
